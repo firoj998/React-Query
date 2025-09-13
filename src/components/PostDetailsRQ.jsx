04e75faaabdddd5bbb934a7d40ba9950c80b0d80
Mohd Firoj <firojqv@gmail.com>
2025-09-13 12:29:41 +0530

    Querying by Id

--- src/components/PostDetailsRQ.jsx
+++ src/components/PostDetailsRQ.jsx
@@ -1,5 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const fetchPostDetails = (postID) => {
+  return axios.get(`http://localhost:3000/posts/${postID}`);
+};
+
 const PostDetailsRQ = () => {
-  return <div>PostDetailsRQ</div>;
+  const { postId } = useParams();
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts", postId],
+    queryFn: () => fetchPostDetails(postId),
+  });
+  if (isLoading) {
+    return <div>Page is loading...</div>;
+  }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+  const { title, body } = data?.data || {};
+  return (
+    <div className="post-details-container">
+      <div className="post-details-title">{title}</div>
+      <div className="post-details-body">{body}</div>
+    </div>
+  );
 };
 
 export default PostDetailsRQ;
--- src/components/PostsReactQuery.jsx
+++ src/components/PostsReactQuery.jsx
@@ -19,16 +19,16 @@ const PostsReactQuery = () => {
   });
   // console.log(data);
   if (isLoading) {
-    return <div>Page is loading...</div>;
+    return <div className="loading">Page is loading...</div>;
   }
   if (isError) {
-    return <div>{error.message}</div>;
+    return <div className="error">{error.message}</div>;
   }
   return (
     <div className="post-list">
       <button onClick={refetch}>Fetch Posts</button>
       {data?.data?.map((post) => (
-        <Link to={`/rq-posts/${post.id}`}>
+        <Link key={post.id} to={`/rq-posts/${post.id}`}>
           <div key={post.id} className="post-item">
             <h3 className="post-title">{post.title}</h3>
             <p className="post-body">{post.body}</p>
