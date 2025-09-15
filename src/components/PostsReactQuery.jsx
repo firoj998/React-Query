f5bda859d336fbe3756947edb80724db16908ec9
Mohd Firoj <firojqv@gmail.com>
2025-09-15 15:18:30 +0530

    Mutation Response

--- src/components/PostsReactQuery.jsx
+++ src/components/PostsReactQuery.jsx
@@ -1,23 +1,60 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+// GET method
+const fetchPosts = () => {
+  return axios.get("http://localhost:3000/posts");
+};
+
+// Post method
+const addPost = (post) => {
+  return axios.post("http://localhost:3000/posts", post);
+};
+
 const PostsReactQuery = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   //posts    ["posts"]
   //posts/1    ["posts", "post.id"]
   //posts/1/comments    ["posts", "post.id", "comments"]  // every single query should have its own query key
 
   const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ["posts"],
-    queryFn: () => {
-      return axios.get("http://localhost:3000/posts");
-    },
+    queryFn: fetchPosts,
     // staleTime: 30000,
     // refetchInterval: 1000,    // polling
     // refetchIntervalInBackground: true,
-    enabled: false, // Disable for query fetching on component mount
+    //enabled: false, // Disable for query fetching on component mount
   });
   // console.log(data);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: addPost,
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries(["posts"]);
+    // },
+    onSuccess: (newData) => {
+      queryClient.setQueryData(["posts"], (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, newData.data],
+        };
+      });
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, body);
+    const post = { title, body };
+    mutate(post);
+    setTitle("");
+    setBody("");
+  };
+
   if (isLoading) {
     return <div className="loading">Page is loading...</div>;
   }
@@ -26,7 +63,20 @@ const PostsReactQuery = () => {
   }
   return (
     <div className="post-list">
-      <button onClick={refetch}>Fetch Posts</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter post  title"
+          value={title}
+        />
+        <input
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="Enter post  body"
+          value={body}
+        />
+        <button type="submit">Post</button>
+      </form>
+
       {data?.data?.map((post) => (
         <Link key={post.id} to={`/rq-posts/${post.id}`}>
           <div key={post.id} className="post-item">
@@ -35,6 +85,7 @@ const PostsReactQuery = () => {
           </div>
         </Link>
       ))}
+      <button onClick={refetch}>Fetch Posts</button>
     </div>
   );
 };
